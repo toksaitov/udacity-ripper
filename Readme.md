e5ea1081d0b85ead1627a49750f65a8f9dd2613d
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2017-12-06 21:42:05 +0600

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+udacity-ripper
+==============
+
+*udacity-ripper* is a python script that uses Selenium to crawl Udacity
+course pages to extract all the lesson YouTube links.
+
+## Prerequisites
+
+* *Python* `>= 2.7`
+* *Selenium* `>= 3.8.0`
+
+# Usage
+
+```bash
+pip install selenium
+python udacity-ripper.py <CLASS URL> <LOGIN> <PASSWORD>
+```
+
+## Licensing
+
+*udacity-ripper* is licensed under the MIT license. See LICENSE for the full
+license text.
+
+## Credits
+
+*udacity-ripper* was created by [Dmitrii Toksaitov](https://github.com/toksaitov).
+
---
+++ udacity-ripper.py
@@ -0,0 +1,88 @@
+import re
+import sys
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+
+if len(sys.argv) < 4:
+    print('Usage: ' + sys.argv[0] + ' <CLASS URL> <LOGIN> <PASSWORD>')
+    sys.exit(1)
+
+login = sys.argv[2]
+password = sys.argv[3]
+implicit_delay = 10
+explicit_delay = 10
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(implicit_delay)
+
+class_url = sys.argv[1]
+driver.get(class_url)
+
+login_field = driver.find_element_by_css_selector("input[type='email']")
+login_field.send_keys(login)
+
+password_field = driver.find_element_by_css_selector("input[type='password']")
+password_field.send_keys(password)
+
+submit_button = driver.find_element_by_css_selector('button')
+submit_button.click()
+
+links = WebDriverWait(driver, explicit_delay).until(
+    expected_conditions.visibility_of_all_elements_located(
+        (By.XPATH, "//a[starts-with(., 'View ')]")
+    )
+)
+for link in links:
+    link.click()
+
+links = WebDriverWait(driver, explicit_delay).until(
+    expected_conditions.visibility_of_all_elements_located(
+        (By.XPATH, '//a[text()="View"] | //a[text()="Continue"] | //a[text()="Start"]')
+    )
+)
+topic_links = []
+for link in links:
+    topic_links.append(link.get_attribute('href'))
+
+print(topic_links)
+print(len(topic_links))
+raw_input('Press the <ENTER> key to continue...')
+
+topic = 1
+for topic_link in topic_links:
+    print('Topic #' + str(topic));
+    topic += 1
+
+    driver.get(topic_link)
+    links = WebDriverWait(driver, explicit_delay).until(
+        expected_conditions.visibility_of_all_elements_located(
+            (By.CSS_SELECTOR, "ol[aria-labelledby='sidebar-concepts-list'] a")
+        )
+    )
+    lesson_links = []
+    for link in links:
+        lesson_links.append(link.get_attribute('href'))
+
+    for lesson_link in lesson_links:
+        driver.get(lesson_link)
+        try:
+            youtube_link = WebDriverWait(driver, explicit_delay / 2).until(
+                expected_conditions.visibility_of_element_located(
+                    (By.CSS_SELECTOR, "iframe[title='YouTube video player']")
+                )
+            )
+
+            video_source = youtube_link.get_attribute('src')
+            video_id = re.search(r"\/([^\/]+)\?", video_source).group(1)
+
+            print('https://www.youtube.com/watch?v=' + video_id)
+        except TimeoutException:
+            pass
+
+raw_input('Press the <ENTER> key to continue...')
+
+driver.quit()
